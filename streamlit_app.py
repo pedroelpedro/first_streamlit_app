ba6110e4db92edb39f9a58c9eaf9418b86bb1de4
Paul Rubin <paularubin@gmail.com>
2022-11-07 15:47:01 +0000

    Update streamlit_app.py
    fruits_to_show - Filter table based on selection

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,11 +7,12 @@ streamlit.header('🍌🥭 Throw Your Own Fruit Smoothie 🥝🍇')
 # Let's put a pick list here so they can pick the fruit they want to include
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.multiselect("Throw some fruits:", list(my_fruit_list.index),['Banana','Grapes','Lime'])
 
 # Display the table
-streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(fruits_to_show)
 
 streamlit.text('Omega 3 & Blackberry Oatmeal Worms')
 streamlit.text('Kale, Beetroot, Spinach & Rocket Milkshake')
