4b3576ba855a0723307e4d5e1fb8d801bab2f983
Paul Rubin <paularubin@gmail.com>
2022-11-07 08:34:48 +0000

    Update streamlit_app.py
    Added silly header

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit
 
 streamlit.header('Breakfast Menu')
+
+streamlit.header('🍌🥭 Throw Your Own Fruit Smoothie 🥝🍇')
 streamlit.text('Omega 3 & Blackberry Oatmeal Worms')
 streamlit.text('Kale, Beetroot, Spinach & Rocket Milkshake')
 streamlit.text('Well-Boiled Extra-Range Swan Eggs with Toasted Poppy Seeds')
