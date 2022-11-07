c3e5a758bf0203781c1f175a57074c507c824735
Paul Rubin <paularubin@gmail.com>
2022-11-07 15:33:22 +0000

    Update streamlit_app.py
    Fixing Multiselect

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,12 +5,12 @@ streamlit.header('Breakfast Menu')
 
 streamlit.header('🍌🥭 Throw Your Own Fruit Smoothie 🥝🍇')
 
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
+
 # Let's put a pick list here so they can pick the fruit they want to include 
 streamlit.multiselect("Throw some fruits:", list(my_fruit_list.index))
 my_fruit_list = my_fruit_list.set_index('Fruity Fruits')
-
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(my_fruit_list)
 
 streamlit.text('Omega 3 & Blackberry Oatmeal Worms')
 streamlit.text('Kale, Beetroot, Spinach & Rocket Milkshake')
